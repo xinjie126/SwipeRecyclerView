1b0ad32c3d62e9e63636093c4019aea2293969cb
zxinjie867 <812815867@qq.com>
2020-04-05 14:55:22 +0800

    init project

---
+++ xSample/src/main/java/com/xin/recyclerview/sample/activity/BaseActivity.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2019 Zou Xinjie
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.xin.recyclerview.sample.activity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.xin.recyclerview.OnItemClickListener;
+import com.xin.recyclerview.SwipeRecyclerView;
+import com.xin.recyclerview.sample.R;
+import com.xin.recyclerview.sample.adapter.BaseAdapter;
+import com.xin.recyclerview.sample.adapter.MainAdapter;
+import com.xin.recyclerview.widget.DefaultItemDecoration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * Created by ZouXinjie on 2019/7/21.
+ */
+public class BaseActivity extends AppCompatActivity implements OnItemClickListener {
+
+    protected Toolbar mToolbar;
+    protected ActionBar mActionBar;
+    protected SwipeRecyclerView mRecyclerView;
+    protected RecyclerView.LayoutManager mLayoutManager;
+    protected RecyclerView.ItemDecoration mItemDecoration;
+
+    protected BaseAdapter mAdapter;
+    protected List<String> mDataList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getContentView());
+
+        mToolbar = findViewById(R.id.toolbar);
+        mRecyclerView = findViewById(R.id.recycler_view);
+
+
+        setSupportActionBar(mToolbar);
+        mActionBar = getSupportActionBar();
+        if (displayHomeAsUpEnabled()) {
+            mActionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        mLayoutManager = createLayoutManager();
+        mItemDecoration = createItemDecoration();
+        mDataList = createDataList();
+        mAdapter = createAdapter();
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.addItemDecoration(mItemDecoration);
+        mRecyclerView.setOnItemClickListener(this);
+    }
+
+    protected int getContentView() {
+        return R.layout.activity_scroll;
+    }
+
+    protected RecyclerView.LayoutManager createLayoutManager() {
+        return new LinearLayoutManager(this);
+    }
+
+    protected RecyclerView.ItemDecoration createItemDecoration() {
+        return new DefaultItemDecoration(ContextCompat.getColor(this, R.color.divider_color));
+    }
+
+    protected List<String> createDataList() {
+        List<String> dataList = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            dataList.add("第" + i + "个Item");
+        }
+        return dataList;
+    }
+
+    protected BaseAdapter createAdapter() {
+        return new MainAdapter(this);
+    }
+
+    @Override
+    public void onItemClick(View itemView, int position) {
+        Toast.makeText(this, "第" + position + "个", Toast.LENGTH_SHORT).show();
+    }
+
+    protected boolean displayHomeAsUpEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return true;
+    }
+
+}
